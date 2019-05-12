6c30b4eb638489f35d17083694cc652e908d5b34
MrWooJ <Hurricanc@gmail.com>
2019-05-12 16:28:07 +0430

    Add All Static Config Files to EnvAttacher Module

--- helper/envAttach.js
+++ helper/envAttach.js
@@ -1,8 +1,28 @@
+const verificationStatus = require('../config/verifiaction.status.js');
+const transactionStatus = require('../config/transaction.status.js');
+const licenseStatus = require('../config/license.status.js');
+const informationType = require('../config/information.type.js');
+const clientType = require('../config/client.type.js');
+const blockchainData = require('../config/blockchain.data.js');
+const environmentType = require('../config/environment.type.js');
+
+const gasPrice = '20';
+const verifiedAdditionTime = 3 * 24 * 60 * 60 * 1000;
+
 module.exports = server => {
   server.vars = {
     config: {
+      verificationStatus,
+      transactionStatus,
+      licenseStatus,
+      informationType,
+      clientType,
+      blockchainData,
+      environmentType
     },
     const: {
+      gasPrice,
+      verifiedAdditionTime
     }
   };
 };
