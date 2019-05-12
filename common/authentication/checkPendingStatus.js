2b6232edec0b5541bdb0a21abbdb9700bcd40164
MrWooJ <Hurricanc@gmail.com>
2019-05-12 16:17:32 +0430

    Implement Check Pending Status Authentications for Authentication Model

---
+++ common/authentication/checkPendingStatus.js
@@ -0,0 +1,32 @@
+const utility = rootRequire('helper/utility');
+let cron = require('cron');
+
+let app = rootRequire('server/server');
+
+module.exports = async Authentication => {
+
+  const vars = app.vars;
+
+  let checkPendingStatus = cron.job('*/10 * * * * *', async () => {
+    let time = utility.getUnixTimeStamp();
+    let authList = await Authentication.find({
+      where: {
+        status: vars.config.verificationStatus.pending
+      }
+    });
+    for (let i = 0; i < authList.length; i++) {
+      let model = authList[i];
+      if (Number(model.date) + Number(model.ttl) < time) {
+        let data = {
+          tryCount: 5,
+          date: time,
+          status: vars.config.verificationStatus.ready
+        };
+        await model.updateAttributes(data);
+      }
+    }
+  });
+  
+  checkPendingStatus.start();
+  
+};
